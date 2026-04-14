5707065f20a0e2c9c889c07e9320b8acfa42d336
eeman_2638 <eeman_2638@dhiraagu.com.mv>
2026-04-14 11:34:21 +0500

    Created Part 1 Folder

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,59 @@
+const App = () => {
+  const course = "half stack application development";
+  const part1 = "fundamantals of react";
+  const exercise1 = 10;
+  const part2 = "using props to pass data";
+  const exercise2 = 7;
+  const part3 = "state of a component";
+  const exercise3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercise1={exercise1}
+        part2={part2}
+        exercise2={exercise2}
+        part3={part3}
+        exercise3={exercise3}
+      />
+      <Total
+        exercise1={exercise1}
+        exercise2={exercise2}
+        exercise3={exercise3}
+      />
+    </div>
+  );
+};
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercise1}
+      </p>
+      <p>
+        {props.part2} {props.exercise2}
+      </p>
+      <p>
+        {props.part3} {props.exercise3}
+      </p>
+    </div>
+  );
+};
+const Total = (props) => {
+  return (
+    <div>
+      number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
+    </div>
+  );
+};
+export default App;
